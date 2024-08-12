5212dad872e9fb18d78d3b5adc17aaf135cc2e38
xsm3z <xsm.zhang@gmail.com>
2024-08-12 17:03:25 -0400

    user controller, user views

---
+++ controllers/users.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+router.get('/', async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    res.render('users/index.ejs', { allUsers });
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+});
+
+router.get('/:userId', async (req, res) => {
+  try {
+    const pageOwner = await User.findById(req.params.userId);
+    res.render('users/show.ejs', {
+      pageOwner
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-const authController = require('./controllers/auth');
-const foodsController = require('./controllers/foods');
-const isSignedIn = require('./middleware/is-signed-in');
-const passUserToView = require('./middleware/pass-user-to-view');
+
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const authController = require('./controllers/auth.js');
+const usersController = require('./controllers/users.js');
+const foodsController = require('./controllers/foods.js');
+const recipesController = require("./controllers/recipes.js");
+const ingredientsController = require("./controllers/ingredients.js");
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -21,11 +25,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-app.use(passUserToView)
-app.use('/auth', authController);
-app.use(isSignedIn);
-app.use('/users/:userId/foods',foodsController);
-
+// app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -40,15 +40,11 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
-
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users', usersController);
+app.use('/users/:userId/foods', foodsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
