556622837e361981a3dd1f2ed0d0e303dfd3f35d
xsm3z <xsm.zhang@gmail.com>
2024-08-12 16:35:08 -0400

    food controller logic and ejs

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,5 +3,72 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+router.get('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    res.render('foods/index.ejs', {
+      pantry: currentUser.pantry,
+    });
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+});
+
+router.get('/new', async (req, res) => {
+  res.render('foods/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+router.get('/:foodId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.pantry.id(req.params.foodId);
+    res.render('foods/edit.ejs', {
+      food: food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+router.put('/:foodId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.pantry.id(req.params.foodId);
+    food.set(req.body);
+    await currentUser.save();
+    res.redirect(
+      `/users/${currentUser._id}/foods/`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+router.delete('/:foodId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.id(req.params.foodId).deleteOne();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
 
 module.exports = router;
